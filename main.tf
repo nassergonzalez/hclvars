17722022543259c65b78f04074f97f69d33c6466
nasser.gonzalez@gmail.com <nasser.gonzalez@harness.io>
2023-06-12 15:38:32 +0100

    bar

--- main.tf
+++ main.tf
@@ -1,17 +1,63 @@
-# Define variables
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
+
 variable "instance_count" {
   type    = number
-  default = 3
 }
 
-# Another variable that references the instance_count variable
-variable "another_variable" {
-  type = string
-  default = "The number of instances is ${var.instance_count}."
+variable "regions" {
+  type    = list(string)
 }
 
-# Output the value of another_variable
-output "output_variable" {
-  value = var.another_variable
+variable "tags" {
+  type = map(string)
+}
+
+variable "allowed_ports" {
+  type    = set(number)
+}
+
+variable "custom_data" {
+  type = object({
+    user = string
+    age  = number
+  })
+}
+
+resource "aws_instance" "example" {
+  count         = var.instance_count
+  instance_type = "t2.micro"
+  ami           = "ami-0c94855ba95c71c99"
+  availability_zone = element(var.regions, count.index % length(var.regions))
+
+  tags = var.tags
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo User: ${var.custom_data.user}",
+      "echo Age: ${var.custom_data.age}",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo Allowed Ports: ${join(", ", var.allowed_ports)}"
+  }
+}
+
+output "instance_ids" {
+  value = aws_instance.example[*].id
+}
+
+output "instance_availability_zones" {
+  value = aws_instance.example[*].availability_zone
 }
 
